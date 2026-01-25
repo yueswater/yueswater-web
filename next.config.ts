517a4b1d4578b93662e12fbc23a6fd931466a4d0
Anthony Sung <sungpinyue@gmail.com>
2026-01-26 02:15:28 +0800

    feat: add parser

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ["res.cloudinary.com"],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
       {
         protocol: "http",
         hostname: "localhost",
